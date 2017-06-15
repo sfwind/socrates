b58abcdc254e6393802a6dca5e410bd60f34d1f7
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-06-15 15:51:50 +0800

    update index url

--- src/main/java/com/iquanwai/job/NotifyJob.java
+++ src/main/java/com/iquanwai/job/NotifyJob.java
@@ -29,8 +29,10 @@ public class NotifyJob {
     private PlanService planService;
     @Autowired
     private TemplateMessageService templateMessageService;
-    @Autowired
-    private RedisUtil redisUtil;
+
+    private static final String INDEX_URL = "/rise/static/plan/main";
+//    @Autowired
+//    private RedisUtil redisUtil;
 
     @Scheduled(cron = "0 0 8 * * ?")
     public void work() {
@@ -41,12 +43,12 @@ public class NotifyJob {
     }
 
 
-    @PostConstruct()
-    public void init(){
-        // 测试上线zk是否成功
-        logger.info("appid------:{}",ConfigUtils.getAppid());
-        logger.info("act------:{}",redisUtil.get("accessToken"));
-    }
+//    @PostConstruct()
+//    public void init(){
+//        // 测试上线zk是否成功
+//        logger.info("appid------:{}",ConfigUtils.getAppid());
+//        logger.info("act------:{}",redisUtil.get("accessToken"));
+//    }
 
 
 
@@ -56,17 +58,17 @@ public class NotifyJob {
             TemplateMessage templateMessage = new TemplateMessage();
             templateMessage.setTouser(improvementPlan.getOpenid());
             templateMessage.setTemplate_id(ConfigUtils.getUnderCloseMsg());
+            templateMessage.setUrl(ConfigUtils.getAppDomain()+INDEX_URL);
 
             Map<String, TemplateMessage.Keyword> data = Maps.newHashMap();
             templateMessage.setData(data);
 
             Problem problem = planService.getProblem(improvementPlan.getProblemId());
 
-            data.put("first",new TemplateMessage.Keyword("这个小课还有3天就到期了：\n"));
+            data.put("first",new TemplateMessage.Keyword("你的以下小课还有3天就到期了：\n"));
             data.put("keyword1",new TemplateMessage.Keyword(problem.getProblem()));
             data.put("keyword2",new TemplateMessage.Keyword(DateUtils.parseDateToString(improvementPlan.getCloseDate())));
-            data.put("remark",new TemplateMessage.Keyword("\n至少做完所有知识理解和巩固练习，才能完成小课，加油加油\n\n" +
-                    "点击下方RISE按钮，快去完成练习吧↓↓↓"));
+            data.put("remark",new TemplateMessage.Keyword("\n抓紧在到期前解锁所有练习吧！"));
 
             templateMessageService.sendMessage(templateMessage);
         });
