cf496162daf899a0571d83e536147ceb3788aa9f
nethunder <17089113140@163.com>
2017-09-17 18:24:36 +0800

    update

--- src/main/java/com/iquanwai/job/NotifyRunningLogin.java
+++ src/main/java/com/iquanwai/job/NotifyRunningLogin.java
@@ -41,7 +41,7 @@ public class NotifyRunningLogin {
     private CustomerMessageLogDao customerMessageLogDao;
 
     //    @Scheduled(cron = "0 30 21 ? * MON-FRI")
-    @Scheduled(cron = "0 0 18 ? * MON-FRI")
+    @Scheduled(cron = "0 35 18 ? * *")
     public void notifyHasRunningPlansLogin() {
         logger.info("开始未登录提醒job");
         List<ImprovementPlan> runningUnlogin = planService.loadRunningUnlogin();
