29f9815e33bb6af37d83f291a2c18f33559247d3
xfduan <1047210087@qq.com>
2017-10-16 16:17:35 +0800

    1

--- src/main/java/com/iquanwai/job/NotifyRiseMemberApplyJob.java
+++ src/main/java/com/iquanwai/job/NotifyRiseMemberApplyJob.java
@@ -31,7 +31,7 @@ public class NotifyRiseMemberApplyJob {
     private void sendRiseMemberApplyMessage() {
         // 优惠券是数据库中日期的 0 点，所以在查询的时候要减 1，但是描述时正常描述
         Date sixDate = DateUtils.beforeDays(new Date(), 6);
-        // customerService.sendRiseMemberApplyMessageByAddTime(sixDate, 1);
+        customerService.sendRiseMemberApplyMessageByAddTime(sixDate, 1);
 
         customerService.sendRiseMemberApplyShortMessageByAddTime(sixDate);
     }
