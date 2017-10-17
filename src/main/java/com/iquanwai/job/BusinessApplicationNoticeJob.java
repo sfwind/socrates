17aa38b59b4983f05a07fe08f24629e1ba0766a7
nethunder <17089113140@163.com>
2017-10-18 00:49:03 +0800

    去掉定时任务，模版消息手动发送

--- src/main/java/com/iquanwai/job/BusinessApplicationNoticeJob.java
+++ src/main/java/com/iquanwai/job/BusinessApplicationNoticeJob.java
@@ -4,10 +4,7 @@ import com.iquanwai.domain.BusinessSchoolService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
 
 /**
  * Created by nethunder on 2017/10/4.
@@ -19,12 +16,12 @@ public class BusinessApplicationNoticeJob {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Scheduled(cron = "0 0 20 * * ?")
-    public void work() {
-        logger.info("商学院申请通知任务开始");
-        businessSchoolService.noticeApplication(new Date());
-        logger.info("商学院申请通知任务开始");
-    }
+//    @Scheduled(cron = "0 0 20 * * ?")
+//    public void work() {
+//        logger.info("商学院申请通知任务开始");
+//        businessSchoolService.noticeApplication(new Date());
+//        logger.info("商学院申请通知任务开始");
+//    }
 
 
 }
