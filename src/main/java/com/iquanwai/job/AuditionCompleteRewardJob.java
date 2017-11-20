ef32811a8961b16cc97259b4ab698d52ed29b553
xfduan <1047210087@qq.com>
2017-11-20 13:01:50 +0800

    change cron

--- src/main/java/com/iquanwai/job/AuditionCompleteRewardJob.java
+++ src/main/java/com/iquanwai/job/AuditionCompleteRewardJob.java
@@ -15,7 +15,7 @@ public class AuditionCompleteRewardJob {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Scheduled(cron = "0 0 21 * * ?")
+    @Scheduled(cron = "0 0 21 ? * THUR")
     public void sendAuditionCompleteReward() {
         logger.info("发送试听课奖学金 job 开始...");
         auditionService.sendAuditionCompleteReward();
