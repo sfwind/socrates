c1801b99b4682bd5aa77665d3f21ca9cea9b4f05
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-12-05 10:52:17 +0800

    update

---
+++ src/main/java/com/iquanwai/domain/po/BusinessSchoolApplicationOrder.java
@@ -0,0 +1,16 @@
+package com.iquanwai.domain.po;
+
+import lombok.Data;
+
+/**
+ * Created by justin on 2017/11/30.
+ */
+@Data
+public class BusinessSchoolApplicationOrder {
+    private Integer id;
+    private Integer profileId;
+    private String openid;
+    private String orderId;
+    private Boolean paid;
+    private Boolean del;
+}
