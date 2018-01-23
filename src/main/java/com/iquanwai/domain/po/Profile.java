8f77ae97a3990ac46e6132e376e59093e6a9f089
xfduan <1047210087@qq.com>
2018-01-23 18:09:48 +0800

    fix unionid

--- src/main/java/com/iquanwai/domain/po/Profile.java
+++ src/main/java/com/iquanwai/domain/po/Profile.java
@@ -8,12 +8,13 @@ import lombok.Data;
 @Data
 public class Profile {
     private Integer id; // 用户 profileId，作为用户唯一区分
-    private String openid;	//用户的标识，对当前公众号唯一
+    private String openid;    //用户的标识，对当前公众号唯一
+    private String unionId;
     private String nickname; //用户的昵称
-    private String city;	//用户所在城市
-    private String country;	//用户所在国家
+    private String city;    //用户所在城市
+    private String country;    //用户所在国家
     private String province; //	用户所在省份
-    private String headimgurl;	//用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
+    private String headimgurl;    //用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
     private String mobileNo;  //手机号
     private String email;  //邮箱
     private String industry; //行业
