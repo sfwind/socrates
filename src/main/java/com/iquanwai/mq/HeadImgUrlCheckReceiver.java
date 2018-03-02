f8b2a7c63b96a68fde344a005faab6618b270948
xfduan <1047210087@qq.com>
2018-03-02 16:26:36 +0800

    1

--- src/main/java/com/iquanwai/mq/HeadImgUrlCheckReceiver.java
+++ src/main/java/com/iquanwai/mq/HeadImgUrlCheckReceiver.java
@@ -10,6 +10,7 @@ import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.concurrent.TimeUnit;
@@ -17,6 +18,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by 三十文
  */
+@Component
 public class HeadImgUrlCheckReceiver {
 
     @Autowired
