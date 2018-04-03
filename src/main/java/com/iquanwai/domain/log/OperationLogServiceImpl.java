82232aab56df8e4ee8a9c25d7c75c9c1bdf4b9b5
nethunder <17089113140@163.com>
2018-04-03 11:36:32 +0800

    update

--- src/main/java/com/iquanwai/domain/log/OperationLogServiceImpl.java
+++ src/main/java/com/iquanwai/domain/log/OperationLogServiceImpl.java
@@ -66,8 +66,8 @@ public class OperationLogServiceImpl implements OperationLogService {
                 properties.put("riseId", profile.getRiseId());
 
                 sa.track(profile.getRiseId(), true, eventName, properties);
-                // TODO 上线前删掉
-                sa.flush();
+                //  上线前删掉
+//                sa.flush();
             } catch (InvalidArgumentException e) {
                 logger.error(e.getLocalizedMessage(), e);
             }
