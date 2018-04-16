2e786bebe311b3674ff6d2f6edec6eabe40c8c61
richardyangcz <richardyangcz@sina.com>
2018-04-16 18:16:44 +0600

    fix project

--- src/main/java/com/iquanwai/job/notify/NotifyRiseMemberExpireJob.java
+++ src/main/java/com/iquanwai/job/notify/NotifyRiseMemberExpireJob.java
@@ -28,7 +28,7 @@ public class NotifyRiseMemberExpireJob {
 
     //@Scheduled(cron = "0 20 21 * * ?")
     //TODO:测试
-    @Scheduled(cron = "0 1 * * * ?")
+    @Scheduled(cron = "0 */1 * * * ?")
     @CatInspect(name = "notifyRiseMemberWillExpired")
     public void work() {
         logger.info("会员即将过期的模板消息提醒任务开始");
