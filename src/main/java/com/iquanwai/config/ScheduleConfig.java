407979bbe334bd2556fbf7589408e9cc22ec4f8b
三十文 <1047210087@qq.com>
2018-04-17 14:23:53 +0800

    fix schedule

---
+++ src/main/java/com/iquanwai/config/ScheduleConfig.java
@@ -0,0 +1,20 @@
+package com.iquanwai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+/**
+ * Created by 三十文
+ */
+@Configuration
+public class ScheduleConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setScheduler(Executors.newScheduledThreadPool(5));
+    }
+
+}
