338cb1cc5b33398ee986db1931b3f510a758ae6f
nethunder <17089113140@163.com>
2018-04-18 12:19:49 +0800

    update

--- src/main/java/com/iquanwai/domain/log/OperationLogServiceImpl.java
+++ src/main/java/com/iquanwai/domain/log/OperationLogServiceImpl.java
@@ -55,8 +55,8 @@ public class OperationLogServiceImpl implements OperationLogService {
     @PostConstruct
     public void init() {
         memberTypeDao.loadAll(MemberType.class).forEach(item -> {
-            classNameMap.put(item.getId(), "className:" + item.getId());
-            groupIdMap.put(item.getId(), "groupId:" + item.getId());
+            classNameMap.put(item.getId(), "className_" + item.getId());
+            groupIdMap.put(item.getId(), "groupId_" + item.getId());
         });
     }
 
